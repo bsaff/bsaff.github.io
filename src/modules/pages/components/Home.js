fd4d6f0bf9744abe7542cf1732da3c1d4043a94f
ben saff <bsaff4@gmail.com>
2019-07-25 14:22:01 -0400

    home content and style

--- src/modules/pages/components/Home.js
+++ src/modules/pages/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from '../css/Home.css';
 
 const Home = () => (
   <div>
-    <h1>Hello – </h1>
-    <p>My name is Ben. I'm a software enginner.</p>
+    <h1>hello – </h1>
+    <p className={styles.text}>My name is Ben. I'm a software enginner. This site is built with React, Babel, & Webpack. I plan to build an alternate version in Vue.js. You can view the source code <a href="https://github.com/bsaff/bsaff.github.io">here.</a></p>
   </div>
 );
 
