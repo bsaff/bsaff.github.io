fcd538e234ea9f755172d75fb7d1dd6fb668daf7
ben saff <bsaff4@gmail.com>
2019-07-26 14:02:18 -0400

    add eslint for react

--- src/modules/pages/components/Home.js
+++ src/modules/pages/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from '../css/Home.css';
 
+const SOURCE_CODE_URL = 'https://github.com/bsaff/bsaff.github.io';
+
 const Home = () => (
   <div>
     <h1>hello – </h1>
-    <p className={styles.text}>My name is Ben. I'm a software enginner. This site is built with React, Babel, & Webpack. I plan to build an alternate version in Vue.js. You can view the source code <a href="https://github.com/bsaff/bsaff.github.io">here.</a></p>
+    <p className={styles.text}>My name is Ben. I&apos;m a software enginner. This site is built with React, Babel, & Webpack. I plan to build an alternate version in Vue.js. You can view the source code <a href={SOURCE_CODE_URL}>here.</a></p>
   </div>
 );
 
