56ea43a80f5d66af76af2f8adab59143992ccfd4
ben saff <bsaff4@gmail.com>
2019-08-16 14:15:44 -0400

    add switch code button for vue app

--- vue-app/pages/home.js
+++ vue-app/pages/home.js
@@ -3,13 +3,17 @@
 const SOURCE_CODE_URL = 'https://github.com/bsaff/bsaff.github.io';
 
 Vue.component('home-page', {
-  data: {
-    SOURCE_CODE_URL
+  methods: {
+    handleClick() {
+      localStorage.removeItem('USE_VUE');
+      location.href = '/';
+    },
   },
   template: `
     <div class="home-page">
     <h1>hello – </h1>
-    <p>My name is Ben. I&apos;m a software enginner. This site is built with React, Babel, & Webpack. I plan to build an alternate version in Vue.js. You can view the source code <a href={{SOURCE_CODE_URL}}>here.</a></p>
+    <p>My name is Ben. I&apos;m a software enginner. This is the Vue.js version of my site. You can view the source code <a href=${SOURCE_CODE_URL}>here.</a></p>
+    <button @click="handleClick">Switch to React</button>
   </div>
   `
 });
