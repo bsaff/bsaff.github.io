3903a2084693e64dbf70399f88095e7c5f8b5a81
ben saff <bsaff4@gmail.com>
2019-08-16 14:40:00 -0400

    add switch to vue.js link

--- react-app/modules/pages/components/Home.js
+++ react-app/modules/pages/components/Home.js
@@ -3,10 +3,18 @@ import styles from '../css/Home.css';
 
 const SOURCE_CODE_URL = 'https://github.com/bsaff/bsaff.github.io';
 
+const handleClick = () => {
+  localStorage.setItem('USE_VUE', true);
+  location.href = '/';
+};
+
 const Home = () => (
   <div>
     <h1>hello – </h1>
-    <p className={styles.text}>My name is Ben. I&apos;m a software enginner. This site is built with React, Babel, & Webpack. I plan to build an alternate version in Vue.js. You can view the source code <a href={SOURCE_CODE_URL}>here.</a></p>
+    <div className={styles.wrapper}>
+      <p className={styles.text}>My name is Ben. I&apos;m a software enginner. This site is built with React, Babel, & Webpack. You can view the source code <a href={SOURCE_CODE_URL}>here.</a></p>
+      <a href="" onClick={handleClick}>Switch to Vue.js</a>
+    </div>
   </div>
 );
 
