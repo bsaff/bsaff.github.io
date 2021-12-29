18ab236fa92065bf0d49e9aca73b05678a3db1de
Benji <bsaff@woopgroup.com>
2021-12-29 14:40:35 -0500

    tweak and copy fat finger fix

--- react-app/modules/pages/components/Home.js
+++ react-app/modules/pages/components/Home.js
@@ -10,7 +10,7 @@ const handleClick = () => {
 const Home = () => (
   <Page>
     <p className={styles.text}>
-      My name is Benjmain Saff. I&apos;m a software engineer based in Philadelphia. I like it here. I&apos;m never more than a short bike ride away from <a href="https://www.paradigmarts.org/" target="_blank" rel="noreferrer">art</a>, <a href='https://utphilly.com/events/' target="_blank" rel="noreferrer">music</a>, <a href="http://royalsushiandizakaya.com/" target="_blank" rel="noreferrer">restaurants</a>, and nature. I enjoy reading books, playing guitar, and ocassionally writing, too. My friends, family, and partner are most important to me – I wouldn&apos;t be much without them. If you are a bookworm too, let&apos;s be friends on <a href='https://www.goodreads.com/user/show/23506884-ben-saff' target="_blank" rel="noreferrer">Goodreads</a>.
+      My name is Benjamin Saff. I&apos;m a software engineer based in Philadelphia. I like it here. I&apos;m never more than a short bike ride away from <a href="https://www.paradigmarts.org/" target="_blank" rel="noreferrer">art</a>, <a href='https://utphilly.com/events/' target="_blank" rel="noreferrer">music</a>, <a href="http://royalsushiandizakaya.com/" target="_blank" rel="noreferrer">restaurants</a>, and nature. I enjoy reading books, playing guitar, and ocassionally writing, too. My friends, family, and partner are most important to me. If you are a bookworm too, let&apos;s be friends on <a href='https://www.goodreads.com/user/show/23506884-ben-saff' target="_blank" rel="noreferrer">Goodreads</a>.
     </p>
     <p>
       I built this site using React.
