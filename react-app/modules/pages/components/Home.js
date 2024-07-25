03624fc144d9319dbc47c5bcdb6ee0cf8ffbf1d8
Ben <bensaff4@gmail.com>
2024-07-25 10:37:40 -0400

    bio update

--- react-app/modules/pages/components/Home.js
+++ react-app/modules/pages/components/Home.js
@@ -10,7 +10,7 @@ const handleClick = () => {
 const Home = () => (
   <Page>
     <p className={styles.text}>
-      Hello! I&apos;m Ben. I&apos;m a software engineer based in Philadelphia. I like it here. I&apos;m never more than a short bike ride away from <a href="https://www.paradigmarts.org/" target="_blank" rel="noreferrer">art</a>, <a href='https://utphilly.com/' target="_blank" rel="noreferrer">music</a>, <a href="http://royalsushiandizakaya.com/" target="_blank" rel="noreferrer">restaurants</a>, <a href="https://www.lemonhillaikido.com/" target="_blank" rel="noreferrer">the dojo</a>, and nature. I enjoy books, playing guitar, studying Aikido, and ocassionally writing. I cherish my community of friends, family, and, most importantly, my wife 👑. If you&apos;re also a bookworm, let&apos;s be friends on <a href='https://www.goodreads.com/user/show/23506884-ben-saff' target="_blank" rel="noreferrer">Goodreads</a>.
+      Hello! I&apos;m Ben. I work as a software engineer and digital product designer and am based in Philadelphia. I like it here; I&apos;m never more than a short bike ride away from <a href="https://www.paradigmarts.org/" target="_blank" rel="noreferrer">art</a>, <a href='https://utphilly.com/' target="_blank" rel="noreferrer">music</a>, <a href="http://royalsushiandizakaya.com/" target="_blank" rel="noreferrer">restaurants</a>, <a href="https://www.lemonhillaikido.com/" target="_blank" rel="noreferrer">the dojo</a>, and nature. I enjoy reading, playing guitar, studying Aikido, and ocassionally writing. Let&apos;s be friends on <a href='https://www.goodreads.com/user/show/23506884-ben-saff' target="_blank" rel="noreferrer">Goodreads</a>.
     </p>
     <p>
       I built this site using React.
