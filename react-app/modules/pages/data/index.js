815eec6283f41a5e0a1c53af4b4c1fc0b562aed3
Ben <bensaff4@gmail.com>
2024-08-19 10:14:06 -0400

    update buy link for my book

--- react-app/modules/pages/data/index.js
+++ react-app/modules/pages/data/index.js
@@ -1,15 +1,15 @@
 export const MusicLinks = [
   {
     label: 'Kintsugi',
-    url: 'https://kintsugimusic.bandcamp.com'
+    url: 'https://kintsugimusic.bandcamp.com',
   },
   {
     label: 'Soul Eater',
-    url: 'https://souleatermusic.bandcamp.com'
+    url: 'https://souleatermusic.bandcamp.com',
   },
   {
     label: 'Maris Lane',
-    url: 'https://marislane.bandcamp.com'
+    url: 'https://marislane.bandcamp.com',
   },
 ];
 
@@ -17,12 +17,12 @@ export const WritingLinks = [
   {
     label: 'Time Makes a Sharp Stone Smooth',
     postLabel: 'Brooklyn Aikikai Journal: Vol 3, No 2',
-    url: 'https://www.brooklynaikikai.com/'
+    url: 'https://www.brooklynaikikai.com/',
   },
   {
     label: 'Living Room Dance with Izzy',
     postLabel: 'Moonstone Press: 2021 Featured Poets Anthology',
-    url: 'https://moonstone-arts-center.square.site/product/2021-featured-poets-anthology/325?cs=true&cst=custom'
+    url: 'https://moonstone-arts-center.square.site/product/2021-featured-poets-anthology/325?cs=true&cst=custom',
   },
   {
     label: 'Calculating Time',
@@ -31,54 +31,53 @@ export const WritingLinks = [
   {
     label: 'Minor League All American Dance Club',
     postLabel: 'Toho Publishing',
-    url: 'https://www.amazon.com/minor-league-american-Publishing-Chapbook/dp/1734499214'
+    url: 'https://www.amazon.com/dp/B0DDGX39LN?ref_=pe_93986420_774957520',
   },
   {
     label: 'So Many Noises',
     postLabel: 'Recenter Press',
-    url: 'http://www.recenterpress.com/issue-one-benjamin-saff.html'
+    url: 'http://www.recenterpress.com/issue-one-benjamin-saff.html',
   },
   {
     label: 'a line describes',
     postLabel: 'Toho Journal: Vol 1, No 1',
-    url: 'https://www.amazon.com/Toho-Journal-Unity-Vol-Literary/dp/1733657533'
+    url: 'https://www.amazon.com/Toho-Journal-Unity-Vol-Literary/dp/1733657533',
   },
   {
     label: 'Considering the Burrito',
     postLabel: 'Marlarkey Books',
-    url: 'http://malarkeybooks.com/critical-analysis'
+    url: 'http://malarkeybooks.com/critical-analysis',
   },
   {
     label: 'Eastern Wisdom for the Broken Heart',
     postLabel: 'Thought Catalog',
-    url: 'https://thoughtcatalog.com/ben-saff/2016/04/5-eastern-wisdoms-to-help-heal-your-broken-heart/'
+    url: 'https://thoughtcatalog.com/ben-saff/2016/04/5-eastern-wisdoms-to-help-heal-your-broken-heart/',
   },
   {
     label: 'In Review: Tokyo Ueno Station by Yū Miri',
     postLabel: 'Asymptote Journal',
-    url: 'https://www.asymptotejournal.com/blog/2019/03/11/misshapen-shards-yu-miris-tokyo-ueno-station-in-review/'
+    url: 'https://www.asymptotejournal.com/blog/2019/03/11/misshapen-shards-yu-miris-tokyo-ueno-station-in-review/',
   },
   {
     label: 'In Review: The Samurai by Shūsaku Endō',
     postLabel: 'Asymptote Journal',
-    url: 'https://www.asymptotejournal.com/blog/2018/10/11/a-journey-of-faith-shusaku-endos-the-samurai-in-review/'
+    url: 'https://www.asymptotejournal.com/blog/2018/10/11/a-journey-of-faith-shusaku-endos-the-samurai-in-review/',
   },
   {
     label: 'In Review: The Emissary by Yoko Tawada',
     postLabel: 'Asymptote Journal',
-    url: 'https://www.asymptotejournal.com/blog/2018/05/24/in-review-the-emissary-by-yoko-tawada/'
-  }
+    url: 'https://www.asymptotejournal.com/blog/2018/05/24/in-review-the-emissary-by-yoko-tawada/',
+  },
 ];
 
 export const EditingLinks = [
   {
     label: 'Rules of Sculpture (2023)',
     postLabel: 'Caroline Furr',
-    url: 'https://www.amazon.com/Rules-Sculpture-Caroline-Furr/dp/B0C6C62676'
+    url: 'https://www.amazon.com/Rules-Sculpture-Caroline-Furr/dp/B0C6C62676',
   },
   {
     label: 'Stand While Lifting (2024, Forthcoming)',
-    postLabel: 'Caroline Furr'
+    postLabel: 'Caroline Furr',
   },
-
 ];
